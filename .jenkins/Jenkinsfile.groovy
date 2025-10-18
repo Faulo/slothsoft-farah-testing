fe6be0a35ff8310a662b49e32fc0bd8585e0587c
Daniel Schulz (Madeline) <faulolio@gmail.com>
2025-10-18 19:55:09 +0200

    build: update files

--- .jenkins/Jenkinsfile.groovy
+++ .jenkins/Jenkinsfile.groovy
@@ -13,18 +13,10 @@ def runTests(def versions) {
 
 			withDockerContainer(image: image, toolName: 'Default') {
 				catchError(stageResult: 'UNSTABLE', buildResult: 'UNSTABLE', catchInterruptions: false) {
-					if (isUnix()) {
-						sh 'install -d -m 0755 /etc/apt/keyrings'
-						sh 'apt update && apt install wget -y'
-						sh 'wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null'
-						sh 'echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | tee -a /etc/apt/sources.list.d/mozilla.list > /dev/null'
-						sh 'apt update && apt install firefox -y'
-					} else {
-						bat 'choco install googlechrome --yes'
-					}
-
 					callShell 'composer update --prefer-lowest'
-					callShell "composer exec phpunit -- --log-junit .reports/${version}.xml"
+					callShell "composer exec phpunit -- --log-junit .reports/${version}-lowest.xml"
+					callShell 'composer update --prefer-stable'
+					callShell "composer exec phpunit -- --log-junit .reports/${version}-stable.xml"
 				}
 			}
 
