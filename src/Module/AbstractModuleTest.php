83692257e43d3d0cba48cb876342a602f0b4ac4b
Daniel Schulz <daniel.schulz@medienreaktor.de>
2026-05-18 08:09:22 +0200

    fix: encoding bug

--- src/Module/AbstractModuleTest.php
+++ src/Module/AbstractModuleTest.php
@@ -30,37 +30,37 @@ use Slothsoft\FarahTesting\Constraints\DOMDocumentIsValidAccordingToSchema;
 use Throwable;
 
 abstract class AbstractModuleTest extends AbstractTestCase {
-    
+
     abstract protected static function getManifestAuthority(): FarahUrlAuthority;
-    
+
     protected function getManifest(): ManifestInterface {
         $cache = TestCache::instance(get_class($this));
-        
+
         return $cache->retrieve('getManifest', function () {
             return Module::resolveToManifest($this->getManifestUrl());
         });
     }
-    
+
     protected function getManifestUrl(): FarahUrl {
         return FarahUrl::createFromComponents(static::getManifestAuthority());
     }
-    
+
     protected function getManifestAsset(): AssetInterface {
         return $this->getManifest()->lookupAsset('/');
     }
-    
+
     protected function getManifestDocument(): DOMDocument {
         return $this->getManifestMethod('getRootElement')->toDocument();
     }
-    
+
     protected function getManifestProperty(string $name) {
         return $this->getObjectProperty($this->getManifest(), $name);
     }
-    
+
     protected function getManifestMethod(string $name) {
         return $this->getObjectMethod($this->getManifest(), $name);
     }
-    
+
     /**
      *
      * @dataProvider assetReferenceProvider
@@ -73,10 +73,10 @@ abstract class AbstractModuleTest extends AbstractTestCase {
             $this->failException($e);
         }
     }
-    
+
     public function assetReferenceProvider(): array {
         $cache = TestCache::instance(get_class($this));
-        
+
         return $cache->retrieve('assetReferenceProvider', function () {
             $provider = [];
             foreach ($this->getReferencedAssetReferences() as $context => $path) {
@@ -89,10 +89,10 @@ abstract class AbstractModuleTest extends AbstractTestCase {
             return $provider;
         });
     }
-    
+
     public function assetReferenceUrlProvider(): array {
         $cache = TestCache::instance(get_class($this));
-        
+
         return $cache->retrieve('assetReferenceUrlProvider', function () {
             $provider = [];
             /**
@@ -112,7 +112,7 @@ abstract class AbstractModuleTest extends AbstractTestCase {
             return $provider;
         });
     }
-    
+
     private function getReferencedAssetReferences(): iterable {
         $manifestDocument = $this->getManifestDocument();
         $nodeList = $manifestDocument->getElementsByTagName('*');
@@ -122,7 +122,7 @@ abstract class AbstractModuleTest extends AbstractTestCase {
             }
         }
     }
-    
+
     private function getContextUrlForManifestNode(DOMElement $node): FarahUrl {
         $path = [];
         while ($node->parentNode instanceof DOMElement) {
@@ -137,7 +137,7 @@ abstract class AbstractModuleTest extends AbstractTestCase {
         $path = FarahUrlPath::createFromSegments($path);
         return $this->getManifestUrl()->withAssetPath($path);
     }
-    
+
     /**
      *
      * @dataProvider assetReferenceUrlProvider
@@ -150,7 +150,7 @@ abstract class AbstractModuleTest extends AbstractTestCase {
             $this->failException($e);
         }
     }
-    
+
     /**
      *
      * @dataProvider assetReferenceUrlProvider
@@ -165,7 +165,7 @@ abstract class AbstractModuleTest extends AbstractTestCase {
             $this->failException($e);
         }
     }
-    
+
     /**
      *
      * @dataProvider assetReferenceUrlProvider
@@ -180,7 +180,7 @@ abstract class AbstractModuleTest extends AbstractTestCase {
             $this->failException($e);
         }
     }
-    
+
     /**
      *
      * @dataProvider assetReferenceUrlProvider
@@ -193,10 +193,10 @@ abstract class AbstractModuleTest extends AbstractTestCase {
             $this->failException($e);
         }
     }
-    
+
     public function assetLocalUrlProvider(): array {
         $cache = TestCache::instance(get_class($this));
-        
+
         return $cache->retrieve('assetLocalUrlProvider', function () {
             $provider = [];
             foreach ($this->getLocalAssetPaths() as $path) {
@@ -213,11 +213,11 @@ abstract class AbstractModuleTest extends AbstractTestCase {
             return $provider;
         });
     }
-    
+
     private function getLocalAssetPaths(): iterable {
         yield from $this->buildPathIndex($this->getManifestAsset());
     }
-    
+
     private function buildPathIndex(AssetInterface $asset): iterable {
         if ($asset->isImportSelfInstruction()) {
             return;
@@ -228,13 +228,13 @@ abstract class AbstractModuleTest extends AbstractTestCase {
         if ($asset->createUrl()->getAssetAuthority() !== static::getManifestAuthority()) {
             return;
         }
-        
+
         yield $asset->getUrlPath();
         foreach ($asset->getAssetChildren() as $childAsset) {
             yield from $this->buildPathIndex($childAsset);
         }
     }
-    
+
     /**
      *
      * @dataProvider assetLocalUrlProvider
@@ -249,7 +249,7 @@ abstract class AbstractModuleTest extends AbstractTestCase {
             $this->failException($e);
         }
     }
-    
+
     /**
      *
      * @dataProvider assetLocalUrlProvider
@@ -264,7 +264,7 @@ abstract class AbstractModuleTest extends AbstractTestCase {
             $this->failException($e);
         }
     }
-    
+
     /**
      *
      * @dataProvider assetLocalUrlProvider
@@ -277,7 +277,7 @@ abstract class AbstractModuleTest extends AbstractTestCase {
             $this->failException($e);
         }
     }
-    
+
     /**
      *
      * @depends      testLocalResultExists
@@ -285,14 +285,14 @@ abstract class AbstractModuleTest extends AbstractTestCase {
      */
     public function testLocalResultIsValidAccordingToSchema(FarahUrl $url): void {
         $mimeType = Module::resolveToResult($url)->lookupMimeType();
-        
+
         if (! MimeTypeDictionary::isXml($mimeType)) {
             $this->markTestSkipped("Won't attempt to validate non-XML resource '$url' with mime type '$mimeType'");
         }
-        
+
         $this->assertThat($url, new DOMDocumentIsValidAccordingToSchema());
     }
-    
+
     /**
      *
      * @dataProvider assetLinkProvider
@@ -300,39 +300,40 @@ abstract class AbstractModuleTest extends AbstractTestCase {
     public function testAssetHasValidLink(string $context, string $link): void {
         try {
             $this->assertNotEquals('', $link, 'Link must not be empty');
-            
+
             if (str_starts_with($link, 'mailto:')) {
                 $this->assertMatchesRegularExpression('~^mailto:.+$~', $link);
                 return;
             }
-            
+
             $linkUri = new Uri($link);
-            
+
             if ($linkUri->getScheme() === 'farah') {
                 $this->assertFileExists((string) $linkUri);
                 return;
             }
-            
+
             if ($linkUri->getHost()) {
                 // external links are assumed to be fine
                 return;
             }
-            
+
             if (preg_match('~^/[^/]+@[^/]+~', $linkUri->getPath())) {
                 $request = MessageFactory::createCustomRequest('GET', $linkUri);
                 $requestStrategy = new LookupAssetStrategy();
                 $linkUri = $requestStrategy->createUrl($request);
-                
+
                 $this->assertFileExists((string) $linkUri);
                 return;
             }
-            
+
             $contextUri = new Uri($context);
             $uri = UriResolver::resolve($contextUri, $linkUri);
-            
+
             if ($uri->getAuthority() === $contextUri->getAuthority() and $uri->getHost() === $contextUri->getHost() and $uri->getPath() === $contextUri->getPath()) {
                 // we point to self
                 if ($id = $linkUri->getFragment()) {
+                    $id = rawurldecode($id);
                     $document = DOMHelper::loadDocument($context);
                     $xpath = DOMHelper::loadXPath($document);
                     $ids = [];
@@ -351,28 +352,28 @@ abstract class AbstractModuleTest extends AbstractTestCase {
             $this->assertLessThan(400, $e->getCode(), sprintf('Resolving link lead to HTTP status "%d":%s%s', $e->getCode(), PHP_EOL, $e->getMessage()));
         }
     }
-    
+
     public function assetLinkProvider(): array {
         $cache = TestCache::instance(get_class($this));
-        
+
         return $cache->retrieve('assetLinkProvider', function () {
             $provider = [];
             $localUrls = $this->assetLocalUrlProvider();
             $crawler = new LinkCrawler(new Set(array_keys($localUrls)));
-            
+
             Module::clearAllCachedAssets();
-            
+
             foreach ($localUrls as $asset => $args) {
                 $url = $args[0];
-                
+
                 try {
                     if (file_exists($asset) and $result = Module::resolveToResult($url)) {
                         $mime = $result->lookupMimeType();
-                        
+
                         if (MimeTypeDictionary::isXml($mime)) {
                             $document = $result->lookupDOMWriter()
                                 ->toDocument();
-                            
+
                             foreach ($crawler->crawlDocument($document) as $reference => $link) {
                                 $provider["$asset $reference"] ??= [
                                     $asset,
@@ -384,9 +385,9 @@ abstract class AbstractModuleTest extends AbstractTestCase {
                 } catch (EmptyTransformationException $e) {
                 }
             }
-            
+
             Module::clearAllCachedAssets();
-            
+
             return $provider;
         });
     }
--- tests/Module/AbstractModuleTestTest.php
+++ tests/Module/AbstractModuleTestTest.php
@@ -3,22 +3,42 @@ declare(strict_types = 1);
 
 namespace Slothsoft\FarahTesting\Module;
 
+use PHPUnit\Framework\ExpectationFailedException;
 use PHPUnit\Framework\TestCase;
 
 /**
  * AbstractModuleTestTest
  *
  * @see AbstractModuleTest
- *
- * @todo auto-generated
  */
 final class AbstractModuleTestTest extends TestCase {
-    
+
     /**
      *
      * @test
      */
     public function testClassExists(): void {
         $this->assertTrue(class_exists(AbstractModuleTest::class), "Failed to load class 'Slothsoft\FarahTesting\Module\AbstractModuleTest'!");
+    }
+
+    public function testAssetHasValidLinkPercentDecodesFragmentId(): void {
+        $context = temp_file(__NAMESPACE__);
+
+        file_put_contents($context, <<<'XML'
+<?xml version="1.0" encoding="UTF-8"?>
+<document>
+    <section id="foo/bar" />
+</document>
+XML
+        );
+
+        try {
+            $sut = $this->getMockForAbstractClass(AbstractModuleTest::class);
+            $sut->testAssetHasValidLink($context, '#foo%2Fbar');
+        } catch (ExpectationFailedException $e) {
+            $this->fail("Should not have thrown: " . PHP_EOL . $e);
+        } finally {
+            unlink($context);
+        }
     }
 }
